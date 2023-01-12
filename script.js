44c77031c437b2946ae4510376e3ee977e8125e2
Amadeu Serras <118369464+nightrunner4@users.noreply.github.com>
2023-01-12 18:35:08 +0100

    Implement primitive working version of calculator
    All the buttons now have event listeners, the calculator works, but
    only when the sequence of inputs is: number, operator, number, equal.
    It's a limited and simple version of the algorithm that's helpful to
    me to find the final one.

--- script.js
+++ script.js
@@ -1,26 +1,65 @@
-function add(a, b) {
-    return a + b;
+function adder(a, b) {
+    return +a + +b;
 };
 
-function subtract(a, b) {
-    return a - b;
+function subtracter(a, b) {
+    return +a - +b;
 };
 
-function multiply(a, b) {
-    return a * b;
+function multiplyer(a, b) {
+    return +a * +b;
 };
 
-function divide(a, b) {
-    return (b == 0) ? '🫣 don\'t be naughty' : a / b;
+function divider(a, b) {
+    return (b == 0) ? '🫣 don\'t be naughty' : +a / +b;
+};
+
+function percenter(a) {
+    return +a / 100;
+};
+
+function changeSign(a) {
+    return +a * (-1);
 };
 
 function operate(operator, a, b) {
-    return operator(a, b);
+    if (operator == '+') {
+        return adder(a, b);
+    } if (operator == '-') {
+        return subtracter(a, b);
+    } if (operator == '*') {
+        return multiplyer(a, b);
+    } if (operator == '/') {
+        return divider(a, b);
+    } if (operator == '%') {
+        return percenter(a);
+    } if (operator == '+ / -') {
+        return changeSign(a);
+    };
 }
 
 // program starts here
+let inputSequence = [];
+const display = document.querySelector('.display');
+const buttons = document.querySelectorAll('button');
+for (const button of buttons) {
+    button.addEventListener('click', function() {
+        let input = event.target.innerHTML;
+        if (input == 'AC') {
+            inputSequence = [];
+            display.innerHTML = '';
+        } else if (input == '=') {
+            let a = inputSequence[0];
+            let operator = inputSequence[1];
+            let b = inputSequence[2];
+            let result = operate(operator, a, b);
+            console.log(result);
+            display.innerHTML = result;
+        } else {
+            inputSequence.push(input);
+            display.innerHTML = input;
+            
+        };
+    });
+}
 
-let a = 2;
-let b = 3;
-
-/* console.log(operate(subtract, a, b)); */
