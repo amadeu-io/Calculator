e8934ef8654f5b27b05066728d50fb3fdb0a5057
Amadeu Serras <118369464+nightrunner4@users.noreply.github.com>
2023-01-13 00:18:56 +0100

    Considerably improve calculator algorithm
    Now the calculator works much more like a real one. Operations can be
    chained indefinetely, showing the result every time a new operator is
    clicked. After the equal sign has been pressed and the result appears
    on screen,  pressing a new number resets the calculator.
    The code probably has a lot of room for improvement and optimisation,
    which is what I'll be working on the next commit, but so far, the
    calculator seems to work pretty well.

--- script.js
+++ script.js
@@ -38,28 +38,73 @@ function operate(operator, a, b) {
     };
 }
 
-// program starts here
+// initial conditions
 let inputSequence = [];
+let numberSequence = '';
+let a = 0;
+let end = false;
+
 const display = document.querySelector('.display');
-const buttons = document.querySelectorAll('button');
-for (const button of buttons) {
-    button.addEventListener('click', function() {
+const numbers = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.operator');
+const equals = document.getElementById('equals');
+const ac = document.getElementById('ac');
+
+let resultant = false;
+display.innerHTML = '0';
+
+for (const number of numbers) {
+    number.addEventListener('click', function() {
+        let input = event.target.innerHTML
+
+        if (end) { // same as AC
+            display.innerHTML = '0';
+            operatorInput = '';
+            numberSequence = '';
+            result = 0;
+            a = 0;
+            end = false;
+        };
+                
+        numberSequence += input;
+        console.log(numberSequence);
+        display.innerHTML = numberSequence;
+
+        
+    });
+};
+
+for (const operator of operators) {
+    operator.addEventListener('click', function() {
         let input = event.target.innerHTML;
-        if (input == 'AC') {
-            inputSequence = [];
-            display.innerHTML = '';
-        } else if (input == '=') {
-            let a = inputSequence[0];
-            let operator = inputSequence[1];
-            let b = inputSequence[2];
-            let result = operate(operator, a, b);
-            console.log(result);
+        if (a) { // && numberSequence != '' ??maybe
+            let result = operate(operatorInput, a, numberSequence);
+            console.log('yess');
             display.innerHTML = result;
-        } else {
-            inputSequence.push(input);
-            display.innerHTML = input;
-            
+            operatorInput = input;
+            a = result;
+            numberSequence = '';
+        } else {  
+            operatorInput = input;
+            a = numberSequence;
+            display.innerHTML = operatorInput;
+            numberSequence = '';
         };
     });
-}
+};
 
+ac.addEventListener('click', function() {    
+    display.innerHTML = '0';
+    operatorInput = '';
+    numberSequence = '';
+    result = 0;
+    a = 0;
+    end = false;
+});
+
+equals.addEventListener('click', function() {
+    let result = operate(operatorInput, a, numberSequence);
+    console.log(result);
+    display.innerHTML = result;
+    end = true;
+});
